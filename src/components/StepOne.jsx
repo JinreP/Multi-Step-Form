c87fb1b3a989d027856ca511996bbffffd6e2f11
25LP3874 25LP3874 <25LP3874@G303-CC0150.local>
2025-09-22 12:37:26 +0800

    nuule

--- src/components/StepOne.jsx
+++ src/components/StepOne.jsx
@@ -1,3 +1,21 @@
 export function Step1() {
- return <div><p >Step one</p></div>;
+  return (
+    <div className="flex flex-col gap-5">
+      <input
+        type="text"
+        className="w-[300px] h-[50px] text-black focus:border-blue-500 border-blue-500 "
+        placeholder="Enter Your First Name"
+      />
+      <input
+        type="text"
+        className="w-[300px] h-[50px] text-black focus:border-blue-500 border-blue-500 "
+        placeholder="Enter Your Last Name"
+      />
+      <input
+        type="text"
+        className="w-[300px] h-[50px] text-black focus:border-blue-500 border-blue-500 "
+        placeholder="Enter Your Username"
+      />
+    </div>
+  );
 }
--- src/components/StepThree.jsx
+++ src/components/StepThree.jsx
@@ -1,3 +1,14 @@
 export function Step3() {
-  return  <div>StepThree</div>;
+  return (
+    <div className="flex flex-col gap-5">
+      <label htmlFor="" className="text-black">
+        Date of birth *
+      </label>
+      <input
+        type="date"
+        className="w-[300px] h-[50px] text-black focus:border-blue-500 border-blue-500 "
+        placeholder="Date of birth"
+      />
+    </div>
+  );
 }
