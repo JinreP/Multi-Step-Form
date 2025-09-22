0ceedca31f50d7adcf2a84dcf040d2a02ce25360
JinreP <subeegod@gmail.com>
2025-09-22 12:37:26 +0800

    nuule

--- src/components/StepTwo.jsx
+++ src/components/StepTwo.jsx
@@ -1,3 +1,26 @@
 export function Step2() {
-  return <div>StepTwo</div>;
+  return (
+    <div className="flex flex-col gap-5">
+      <input
+        type="text"
+        className="w-[300px] h-[50px] text-black focus:border-blue-500 border-blue-500 "
+        placeholder="Enter Your Email"
+      />
+      <input
+        type="number"
+        className="w-[300px] h-[50px] text-black focus:border-blue-500 border-blue-500 "
+        placeholder="Enter Your Phone Number"
+      />
+      <input
+        type="password"
+        className="w-[300px] h-[50px] text-black focus:border-blue-500 border-blue-500 "
+        placeholder="Enter Your Password"
+      />
+      <input
+        type="password"
+        className="w-[300px] h-[50px] text-black focus:border-blue-500 border-blue-500 "
+        placeholder="Confirm You're Password"
+      />
+    </div>
+  );
 }
