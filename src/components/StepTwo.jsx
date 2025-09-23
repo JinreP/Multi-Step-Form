d8a196106e132642733fe2a2eb56ede0fca25242
JinreP <subeegod@gmail.com>
2025-09-23 21:41:03 +0800

    merged

--- src/components/StepTwo.jsx
+++ src/components/StepTwo.jsx
@@ -1,30 +1,9 @@
+import { userAgent } from "next/server";
+
 export function Step2(props) {
   const { data, setData } = props;
 
-  let error = "";
-  let numberError = "";
-  let passError = "";
-  let passError2 = "";
-  if (data.email === "") {
-    error = "Email is required";
-  } else if (!data.email.includes("@gmail.com")) {
-    error = "Type you're real email!!";
-  }
-  if (data.number === "") {
-    numberError = "Phone number is required";
-  } else if (data.number.length > 8) {
-    numberError = "type you're real phone number";
-  }
-  if (data.password === "") {
-    passError = "Password is required";
-  }
-  if (data.confirmPass === "") {
-    passError2 = "Confirm you're password";
-  } else if (data.password !== "" && data.password === data.confirmPass) {
-    passError2 = "Password matches";
-  } else {
-    passError2 = "Password is not matching !!";
-  }
+  const shortUsername = Username.length > 0 && Username.length < 3;
 
   return (
     <div className="flex flex-col gap-5 items-start justify-start">
@@ -38,6 +17,8 @@ export function Step2(props) {
       />
       {error && <span className="text-red-500 text-sm mt-2">{error}</span>}
 
+      {error && <span className="text-red-500 text-sm mt-2">{error}</span>}
+
       <label htmlFor="">Phone number *</label>
       <input
         type="number"
@@ -46,9 +27,6 @@ export function Step2(props) {
         value={data.number}
         onChange={(e) => setData({ ...data, number: e.target.value })}
       />
-      {numberError && (
-        <span className="text-red-500 text-sm mt-2">{numberError}</span>
-      )}
 
       <label htmlFor="">Password *</label>
       <input
@@ -58,9 +36,6 @@ export function Step2(props) {
         value={data.password}
         onChange={(e) => setData({ ...data, password: e.target.value })}
       />
-      {passError && (
-        <span className="text-red-500 text-sm mt-2">{passError}</span>
-      )}
       <label htmlFor="">Password confirm *</label>
       <input
         type="password"
@@ -69,9 +44,6 @@ export function Step2(props) {
         value={data.confirmPass}
         onChange={(e) => setData({ ...data, confirmPass: e.target.value })}
       />
-      {passError2 && (
-        <span className="text-red-500 text-sm mt-2">{passError2}</span>
-      )}
     </div>
   );
 }
